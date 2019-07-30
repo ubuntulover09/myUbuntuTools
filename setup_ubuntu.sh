018fdcf4e721eae972789601691b8e1dfda9f708
ardhendu <42368233+ardhendugarai@users.noreply.github.com>
2019-07-30 20:04:04 +0530

    Add files via upload

---
+++ setup_ubuntu.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#My Daily use ubuntu tools
+#Author : Ardhendu Garai
+
+ 
+cat  << EOF 
+
+ ▄▄▄       ██▀███  ▓█████▄  ██░ ██ ▓█████  ███▄    █ ▓█████▄  █    ██ 
+▒████▄    ▓██ ▒ ██▒▒██▀ ██▌▓██░ ██▒▓█   ▀  ██ ▀█   █ ▒██▀ ██▌ ██  ▓██▒
+▒██  ▀█▄  ▓██ ░▄█ ▒░██   █▌▒██▀▀██░▒███   ▓██  ▀█ ██▒░██   █▌▓██  ▒██░
+░██▄▄▄▄██ ▒██▀▀█▄  ░▓█▄   ▌░▓█ ░██ ▒▓█  ▄ ▓██▒  ▐▌██▒░▓█▄   ▌▓▓█  ░██░
+ ▓█   ▓██▒░██▓ ▒██▒░▒████▓ ░▓█▒░██▓░▒████▒▒██░   ▓██░░▒████▓ ▒▒█████▓ 
+ ▒▒   ▓▒█░░ ▒▓ ░▒▓░ ▒▒▓  ▒  ▒ ░░▒░▒░░ ▒░ ░░ ▒░   ▒ ▒  ▒▒▓  ▒ ░▒▓▒ ▒ ▒ 
+  ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ▒  ▒  ▒ ░▒░ ░ ░ ░  ░░ ░░   ░ ▒░ ░ ▒  ▒ ░░▒░ ░ ░ 
+  ░   ▒     ░░   ░  ░ ░  ░  ░  ░░ ░   ░      ░   ░ ░  ░ ░  ░  ░░░ ░ ░ 
+      ░  ░   ░        ░     ░  ░  ░   ░  ░         ░    ░       ░     
+                    ░                                 ░               
+
+                                                        
+EOF
+
+
+
+sudo apt-get install python -y
+sudo apt-get install firefox -y
+sudo apt-get install telegram-desktop -y
+sudo apt-get install mat -y
+sudo apt-get install ghex -y
+sudo apt-get install gufw -y
+sudo apt-get install vokoscreen -y
+sudo apt-get install nmap -y
+sudo apt-get install aircrack-ng -y
+sudo apt-get install wireshark -y
+sudo apt-get install ntopng -y
+sudo apt-get install nethogs -y
+sudo apt-get install nikto -y
+sudo apt-get install htop -y
+sudo apt-get install hexchat -y
+sudo apt-get install handbrake-gtk -y
+sudo apt-get install gimp -y
+sudo apt-get install gdebi -y 
+sudo apt-get install alien -y
+sudo apt-get install clamtk -y
+sudo apt-get install edb-debugger -y
+sudo apt-get install gprolog -y
+sudo apt-get install hydra -y
+sudo apt-get install iftop -y
+sudo apt-get install kdenlive -y
+sudo apt-get install nikto -y
+sudo apt-get install openjdk-8-jdk-headless -y
+sudo apt-get install openssh-server -y
+sudo apt-get install openssh-client -y
+sudo apt-get install shotwell -y
+sudo apt-get install gthumb -y
+sudo apt-get install synaptic -y
+sudo apt-get install vlc -y
+sudo apt-get install vsftp -y
+sudo apt-get install -y anydesk leafpad gthumb gtkhash nomacs mypaint magnus onboard pyspread xfce4-terminal handbrake-gtk hexchat mpv gparted gnome-contacts gnome-tweaks 
+
+sudo apt-get install shellcheck -y
+
+echo FINISH!!! Now ENJOY!!!
