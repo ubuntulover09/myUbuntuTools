c5738d0322e09c4b29c4d61eb0c783db5644a6d9
ardhendu <42368233+ubuntulover09@users.noreply.github.com>
2019-11-07 21:40:48 +0530

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -20,43 +20,75 @@ cat  << EOF
 EOF
 
 
-
-sudo apt-get install python -y
+sudo apt-get install ubuntu-restricted-extras -y
+sudo apt-get install aircrack-ng -y
+sudo apt-get install alien -y
+sudo apt-get install clamtk -y
+sudo apt-get install curl -y
 sudo apt-get install firefox -y
-sudo apt-get install telegram-desktop -y
+sudo apt install ffmpeg -y
 sudo apt-get install mat -y
 sudo apt-get install ghex -y
 sudo apt-get install gufw -y
-sudo apt-get install vokoscreen -y
+sudo apt-get install gimp -y
+sudo apt-get install gdebi -y 
+sudo apt-get install gthumb -y
+sudo apt-get install git -y
+sudo apt-get install gpa -y
 sudo apt-get install nmap -y
-sudo apt-get install aircrack-ng -y
-sudo apt-get install wireshark -y
 sudo apt-get install ntopng -y
 sudo apt-get install nethogs -y
 sudo apt-get install nikto -y
 sudo apt-get install htop -y
 sudo apt-get install hexchat -y
 sudo apt-get install handbrake-gtk -y
-sudo apt-get install gimp -y
-sudo apt-get install gdebi -y 
-sudo apt-get install alien -y
-sudo apt-get install clamtk -y
 sudo apt-get install edb-debugger -y
 sudo apt-get install gprolog -y
 sudo apt-get install hydra -y
 sudo apt-get install iftop -y
+sudo apt-get install inkscape -y
 sudo apt-get install kdenlive -y
 sudo apt-get install nikto -y
 sudo apt-get install openjdk-8-jdk-headless -y
 sudo apt-get install openssh-server -y
 sudo apt-get install openssh-client -y
+sudo apt-get install python -y
 sudo apt-get install shotwell -y
-sudo apt-get install gthumb -y
 sudo apt-get install synaptic -y
+sudo apt-get install telegram-desktop -y
 sudo apt-get install vlc -y
+sudo apt-get install vim -y
+sudo apt-get install vokoscreen -y
 sudo apt-get install vsftp -y
-sudo apt-get install -y anydesk leafpad gthumb gtkhash nomacs mypaint magnus onboard pyspread xfce4-terminal handbrake-gtk hexchat mpv gparted gnome-contacts gnome-tweaks 
-
+sudo apt-get install wireshark -y
+sudo apt-get install gthumb -y
+sudo apt-get install gnome-tweaks -y
+sudo apt-get install gparted -y
+## sudo apt-get install -y anydesk leafpad  gtkhash nomacs mypaint magnus onboard pyspread xfce4-terminal handbrake-gtk hexchat mpv  gnome-contacts  
 sudo apt-get install shellcheck -y
+sudo apt-get install folder-color -y
+apt-cache search nemo
+
+#####    nautilus extension  #####
+sudo apt-get install nautilus-image-converter -y
+sudo apt install nautilus-extension-gnome-terminal -y
+sudo apt-get install -y nautilus-admin -y
+sudo apt-get install -y nautilus-wipe 
+sudo apt-get install -y nautilus-gtkhash                # nautilus extension for computing checksums and more using gtkhash
+sudo apt-get install -y nautilus-hide                   # Extension for Nautilus to hide files without renaming them
+sudo apt-get install -y nautilus-admin                  # Extension for Nautilus to do administrative operations
+sudo apt-get install -y nautilus-compare                # Context menu comparison extension for Nautilus file manager
+sudo apt-get install -y nautilus-jpgcompress            # An extension to compress/optimize jpeg files
+sudo apt-get install -y nautilus-locompress             # An extension to compress/optimize LibreOffice odt files
+sudo apt-get install -y nautilus-loextract              # An extension to extract images from LibreOffice files
+sudo apt-get install -y nautilus-iconify                # An extension for Nautilus to create images for Android
+sudo apt-get install -y nautilus-document-converter     # An extension for Nautilus to convert document files.
+sudo apt-get install -y nautilus-lxsplit                # An extension for Nautilus to split and join files
+sudo apt-get install -y nautilus-pdf-tools              # This is a extension for Nautilus to manipulate pdf files
+sudo apt-get install -y nautilus-pngcompress            # An extension to compress png files
+sudo apt-get install -y nautilus-printit                # An extension for Nautilus to print images
+sudo apt-get install -y nautilus-reduceimages           # An extension to reduce image
+
+nautilus -q
 
 echo FINISH!!! Now ENJOY!!!
