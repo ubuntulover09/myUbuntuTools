d9c2df545cc92788987e038f06d67652581fab69
ardhendu <42368233+ubuntulover09@users.noreply.github.com>
2020-01-08 10:42:59 +0530

    Create essential.sh

---
+++ essential.sh
@@ -0,0 +1,12 @@
+
+sudo apt-get install ubuntu-restricted-extras -y
+sudo apt-get install gufw gimp gdebi gthumb git gpa vim vlc mat clamtk nmap gparted firefox ffmpeg curl  git -y
+sudo apt-get install sublime-text
+ 
+sudo apt-get install unity-tweak-tool torbrowser-launcher synaptic  lsb chrome-gnome-shell   bleachbit -y
+
+sudo add-apt-repository ppa:atareao/telegram -y
+sudo apt-get update 
+sudo apt-get install telegram -y
+
+sudo apt-get upgrade
